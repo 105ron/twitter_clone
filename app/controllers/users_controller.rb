a013f2c2eb12a2641dd1e50a224c809a6eb44ebf
rhys <rbrooker105@gmail.com>
2016-10-06 17:50:04 +1100

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
 
   def index
+    #@users = User.where(activated: true).paginate(page: params[:page])
     @users = User.paginate(page: params[:page])
   end
 
@@ -18,6 +19,8 @@ class UsersController < ApplicationController
 
 	def show
     @user = User.find(params[:id])
+    redirect_to root_url 
+
     #debugger  #byebug gem
   end  
 
@@ -25,10 +28,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-    	flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-      #redirect_to user_url(@user) #same as redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
