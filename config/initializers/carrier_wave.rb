a62132234ec3625ecf411d1e8bfa0499d18db0e9
rhys <rbrooker105@gmail.com>
2016-10-12 17:42:48 +1100

    add path_style to carrier_wave - S3 bug fixes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,8 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-	  :region                => ENV['S3_REGION']
-	  :endpoint              => 'http://s3.amazonaws.com'
+	  :region                => ENV['S3_REGION'],
+	  :path_style => true
     }
     config.fog_directory     =  ENV['S3_BUCKET']
 	  
