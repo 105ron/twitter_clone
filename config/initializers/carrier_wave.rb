027bf419d4a9b87d24d6704e3679c67be97a384d
rhys <rbrooker105@gmail.com>
2016-10-12 17:37:02 +1100

    add endpoint to carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,10 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
 	  :region                => ENV['S3_REGION']
+	  :endpoint              => 'http://s3.amazonaws.com'
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+	  
+
   end
 end
